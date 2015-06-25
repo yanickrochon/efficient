b5f4eec5e335c9e60d9253f185dc74aa8246b0f0
yanickrochon <yanick.rochon@gmail.com>
2015-06-25 16:05:35 -0400

    Compiler draft

--- test/lib/compiler.test.js
+++ test/lib/compiler.test.js
@@ -5,7 +5,70 @@ describe('Test compiler', function () {
   var Compiler = require('../../lib/compiler');
 
 
-  describe('Compiling from data', function () {
+  describe('Text-only templates', function () {
+
+    it('should compile single text segment');
+
+    it('should optimize consecutive text segments');
+
+  });
+
+
+  describe('Output segments', function () {
+
+    it('should compile single segment');
+
+    it('should compile more segments');
+
+    it('should optimize');
+
+    it('should integrate with other segments');
+
+  });
+
+
+  describe('Conditional segments', function () {
+
+    it('should compile single segment');
+
+    it('should compile with else segment');
+
+    it('should integrate with other segments');
+
+  });
+
+
+  describe('Switch segments', function () {
+
+    it('should compile single segment');
+
+    it('should compile with more segments');
+
+    it('should integrate with other segments');
+
+  });
+
+
+  describe('Iterator segments', function () {
+
+    it('should compile single segment');
+
+    it('should integrate with other segments');
+
+  });
+
+
+  describe('Parsed template', function () {
+
+    it('should compile', function () {
+      var parsed = require('../fixtures/simple-2.eft');
+
+      var fn = Compiler.compile(parsed);
+
+
+      console.log("*** COMPILED", fn && fn.toString());
+
+    });
 
   });
 

97db8d37d2a2e0813f9463b6a4691b5cbac98ca6
yanickrochon <yanick.rochon@gmail.com>
2015-07-09 16:03:51 -0400

    Added beautifier to compiler. Implemented conditional segment

--- test/lib/compiler.test.js
+++ test/lib/compiler.test.js
@@ -4,6 +4,9 @@ describe('Test compiler', function () {
 
   var Compiler = require('../../lib/compiler');
 
+  Compiler.BEAUTIFY = true;   // DEBUG
+
+
   function execTemplate(tpl, data) {
     var Context = require('../../lib/context');
     var ctx = new Context(data);
@@ -11,12 +14,14 @@ describe('Test compiler', function () {
       raw: [],
       buffer: ''
     };
-    var print = function print(str) {
-      output.raw.push(str);
-      output.buffer += str;
+    var engine = {
+      out: function out(str) {
+        output.raw.push(str);
+        output.buffer += str;
+      }
     };
 
-    return tpl(ctx,print,undefined).then(function () {
+    return tpl(engine,ctx).then(function () {
       return output;
     });
   }
@@ -75,9 +80,113 @@ describe('Test compiler', function () {
 
   describe('Output segments', function () {
 
-    it('should compile single segment');
+    it('should compile single segment', function (done) {
+      var parsed = [
+        {
+          "type": "output",
+          "value": {
+            "context": "foo",
+            "content": [
+              {
+                "type": "context",
+                "value": "ctx.getContext(\"add\").data",
+                "text": "add"
+              },
+              {
+                "type": "number",
+                "value": 2,
+                "text": "2"
+              },
+              {
+                "type": "operator",
+                "value": "+",
+                "text": "+"
+              }
+            ]
+          },
+          "text": "{{foo\\ add + 2}}"
+        }
+      ];
+      var fn = Compiler.compile(parsed);
 
-    it('should compile more segments');
+      execTemplate(fn, {
+        foo: {
+          add: 10
+        }
+      }).then(function (output) {
+        output.buffer.should.equal('12');
+      }).then(done).catch(done);
+    });
+
+    it('should compile more segments', function (done) {
+      var parsed = [
+        {
+          "type": "output",
+          "value": {
+            "context": "foo",
+            "content": [
+              {
+                "type": "context",
+                "value": "ctx.getContext(\"add\").data",
+                "text": "add"
+              },
+              {
+                "type": "number",
+                "value": 2,
+                "text": "2"
+              },
+              {
+                "type": "operator",
+                "value": "+",
+                "text": "+"
+              }
+            ]
+          },
+          "text": "{{foo\\ add + 2}}"
+        },
+        {
+          "type": "text",
+          "value": " ",
+          "text": " "
+        },
+        {
+          "type": "output",
+          "value": {
+            "context": "bar",
+            "content": [
+              {
+                "type": "context",
+                "value": "ctx.getContext(\"mul\").data",
+                "text": "mul"
+              },
+              {
+                "type": "number",
+                "value": 10,
+                "text": "10"
+              },
+              {
+                "type": "operator",
+                "value": "*",
+                "text": "*"
+              }
+            ]
+          },
+          "text": "{{foo\\ mul * 10}}"
+        }
+      ];
+      var fn = Compiler.compile(parsed);
+
+      execTemplate(fn, {
+        foo: {
+          add: 10
+        },
+        bar: {
+          mul: 5
+        }
+      }).then(function (output) {
+        output.buffer.should.equal('12 50');
+      }).then(done).catch(done);
+    });
 
     it('should optimize');
 
@@ -88,12 +197,86 @@ describe('Test compiler', function () {
 
   describe('Conditional segments', function () {
 
-    it('should compile single segment');
+    it('should compile single segment', function (done) {
+      var parsed = require('../fixtures/segments/conditional1.eft');
+      var fn = Compiler.compile(parsed);
+      var tests = [
+      [    // false
+        false, null, undefined, 0, ''
+      ],[  // true
+        true, {}, [], function () {}, -1, 1, /./
+      ]];
 
-    it('should compile with else segment');
+      Promise.all(tests.map(function(values, truthy) {
+        return Promise.all(values.map(function (value) {
+          var data = {
+            value: value
+          };
 
-    it('should integrate with other segments');
+          return execTemplate(fn, data);
+        })).then(function (res) {
+          res.map(function (output) { return output.buffer; }).should.eql(values.map(function () {
+            return truthy ? 'Hello World': '';
+          }));  
+        });
+      })).then(function () { done(); }).catch(done);
+    });
 
+    it('should compile with else segment', function (done) {
+      var parsed = require('../fixtures/segments/conditional2.eft');
+      var fn = Compiler.compile(parsed);
+      var tests = [
+      [    // false
+        false, null, undefined, 0, ''
+      ],[  // true
+        true, {}, [], function () {}, -1, 1, /./
+      ]];
+
+      Promise.all(tests.map(function(values, truthy) {
+        return Promise.all(values.map(function (value) {
+          var data = {
+            value: value
+          };
+
+          return execTemplate(fn, data);
+        })).then(function (res) {
+          res.map(function (output) { return output.buffer; }).should.eql(values.map(function () {
+            return truthy ? 'Hello World': 'Good Bye';
+          }));  
+        });
+      })).then(function () { done(); }).catch(done);
+    });
+
+    it('should integrate with other segments', function (done) {
+      var parsed = require('../fixtures/segments/conditional3.eft');
+      var fn = Compiler.compile(parsed);
+      var tests = [
+      [    // false
+        false, null, undefined, 0, ''
+      ],[  // true
+        true, {}, [], function () {}, -1, 1, /./
+      ]];
+      var PREVAL = '>>>';
+      var POSTVAL = '<<<';
+
+      Promise.all(tests.map(function(values, truthy) {
+        return Promise.all(values.map(function (value) {
+          var data = {
+            pre: PREVAL,
+            foo: {
+              value: value,
+            },
+            post: POSTVAL
+          };
+
+          return execTemplate(fn, data);
+        })).then(function (res) {
+          res.map(function (output) { return output.buffer; }).should.eql(values.map(function () {
+            return PREVAL + (truthy ? 'Hello World': 'Good Bye') + POSTVAL;
+          }));  
+        });
+      })).then(function () { done(); }).catch(done);
+    });
   });
 
 
@@ -138,8 +321,10 @@ describe('Test compiler', function () {
 
       Promise.all(values.map(function(value, index) {
         var data = {
-          index: index,
-          value: value
+          foo: {
+            index: index,
+            value: value
+          }
         };
 
         return execTemplate(fn, data);
@@ -166,7 +351,7 @@ describe('Test compiler', function () {
 
   describe('Parsed template', function () {
 
-    it('should compile', function (done) {
+    it('should compile simple template', function (done) {
       var parsed = require('../fixtures/simple-2.eft');
       var data = {
         name: 'John',
